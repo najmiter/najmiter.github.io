74da9719efcc1d3b607c2ad5d0e4360e44abe184
Najam <85332859+najmiter@users.noreply.github.com>
2024-03-16 23:30:37 +0500

    ramadan karim bro

--- main.js
+++ main.js
@@ -13,3 +13,12 @@ poo.addEventListener("click", function (click) {
         poo_img.setAttribute("src", "");
     }, 3 * 1000);
 });
+
+document.addEventListener("click", function (click) {
+    if (
+        !click.target.contains(mobile_navbar) &&
+        !click.target.contains(burger)
+    ) {
+        hide_mobile_navbar();
+    }
+});
--- scripts/handlers.js
+++ scripts/handlers.js
@@ -1,3 +1,4 @@
+const navbar = document.getElementById("navbar");
 close_preview.addEventListener("click", handle_close_preview);
 
 document.addEventListener("keydown", function (key) {
@@ -8,7 +9,7 @@ document.addEventListener("keydown", function (key) {
 
 let previous_scroll = 0;
 window.addEventListener("scroll", function () {
-    const navbar = document.getElementById("navbar");
+    hide_mobile_navbar();
     const threshold = 500;
 
     if (
@@ -30,3 +31,29 @@ window.addEventListener("scroll", function () {
 
     previous_scroll = window.scrollY;
 });
+
+const burger = document.getElementById("burger");
+const mobile_navbar = document.querySelector(".mobile-navbar");
+burger.addEventListener("click", function (click) {
+    mobile_navbar.style.display = "grid";
+
+    mobile_navbar.style.top = click.clientY + "px";
+    mobile_navbar.style.left = click.clientX - 225 + "px";
+    mobile_navbar.classList.remove("fadeout");
+    mobile_navbar.classList.add("fadein");
+});
+
+mobile_navbar.addEventListener("click", function (click) {
+    if (click.target.closest("li")) {
+        click.target.querySelector("a")?.click();
+        hide_mobile_navbar();
+    }
+});
+
+function hide_mobile_navbar() {
+    mobile_navbar.classList.remove("fadein");
+    mobile_navbar.classList.add("fadeout");
+    setTimeout(() => {
+        mobile_navbar.style.display = "none";
+    }, 500);
+}
