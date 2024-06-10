3f0afc6d9d57284b2eff4a4a271a2553540fd2ed
najmiter <realnajmiter@gmail.com>
2024-06-10 22:48:35 +0500

    ye to ni kaatny thy bro 😩

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,30 +13,22 @@ const plusJakartaSans = Plus_Jakarta_Sans({
 });
 
 export default function Navbar() {
-    const [padding, setPadding] = useState("py-4");
-
-    useEffect(function () {
-        document.addEventListener("scroll", function () {
-            if (window.scrollY > 20) {
-                setPadding("py-2");
-            } else setPadding("py-4");
-        });
-    }, []);
-
     return (
         <Wrapper className="max-w-full p-0">
             <nav
-                className={`sticky top-0 bg-[#222222b3] px-5 backdrop-blur-3xl ${padding !== "py-4" && "text-stone-300"}`}
+                className={`sticky top-0 bg-[#222222b3] px-5 backdrop-blur-3xl text-stone-200`}
             >
                 <div
-                    className={`flex items-center justify-between max-w-[70rem] mx-auto transition-all duration-300 ${padding}`}
+                    className={`flex items-center justify-between max-w-[70rem] mx-auto transition-all duration-300 py-3`}
                 >
-                    <Image
-                        src="/logo.svg"
-                        width={padding === "py-4" ? 40 : 35}
-                        height={padding === "py-4" ? 40 : 35}
-                        alt="najmiter logo"
-                    />
+                    <Link href="/">
+                        <Image
+                            src="/logo.svg"
+                            width={35}
+                            height={35}
+                            alt="najmiter logo"
+                        />
+                    </Link>
                     <ul
                         className={cn(
                             "flex gap-3 sm:gap-20 font-light cursor-pointer text-sm sm:text-base",
