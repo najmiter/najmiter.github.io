d4859ce60bcb2cd0fc91c996ab6d3513316dd753
Najam <realnajmiter@gmail.com>
2025-05-30 17:35:27 +0500

    fix: type error

--- src/app/api/activity/route.ts
+++ src/app/api/activity/route.ts
@@ -1,7 +1,7 @@
 import { getGitHubActivity } from '@/lib/server';
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
   const year = req.nextUrl.searchParams.get('year') ?? new Date().getFullYear();
-  return await getGitHubActivity(+year);
+  return NextResponse.json(await getGitHubActivity(+year));
 }
