2bb4cfa6ba34d28c4f5abe14b7ea10f78f25aeeb
Najam <realnajmiter@gmail.com>
2025-05-31 23:03:16 +0500

    chore: allow all origins

--- src/app/api/activity/route.ts
+++ src/app/api/activity/route.ts
@@ -3,5 +3,11 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
   const year = req.nextUrl.searchParams.get('year') ?? new Date().getFullYear();
-  return NextResponse.json(await getGitHubActivity(+year));
+  return NextResponse.json(await getGitHubActivity(+year), {
+    status: 200,
+    headers: {
+      'Cache-Control': 'public, max-age=3600',
+      'Access-Control-Allow-Origin': '*',
+    },
+  });
 }
