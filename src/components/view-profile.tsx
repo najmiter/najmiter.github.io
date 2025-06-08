401a14adcb1b19d5609f281d2c07fe2b7a145389
Najam <realnajmiter@gmail.com>
2025-06-08 14:41:27 +0500

    maybe fix: timezone

--- src/components/view-profile.tsx
+++ src/components/view-profile.tsx
@@ -1,9 +1,15 @@
 'use client';
 
+import React from 'react';
+
 export default function ViewProfile() {
+  const [timezone] = React.useState(
+    Intl.DateTimeFormat().resolvedOptions().timeZone
+  );
+
   return (
     <a
-      href={`/api/me?timezone=${Intl.DateTimeFormat().resolvedOptions().timeZone}`}
+      href={`/api/me?timezone=${timezone}`}
       target="_blank"
       className="hover:text-blue-400 transition-colors"
     >
