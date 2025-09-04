5d69830ee4a9dde0f7c7efdd75ee874be9721f90
Najam <realnajmiter@gmail.com>
2025-09-04 13:33:11 +0500

    chore: seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -94,7 +94,24 @@ export default async function RootLayout({ children }: React.PropsWithChildren) 
           dangerouslySetInnerHTML={{
             __html: JSON.stringify({
               '@context': 'https://schema.org',
-              '@type': 'profile',
+              '@type': 'website',
+              name: 'Najam ul Hassan',
+              url: 'https://www.najmiter.dev/',
+              potentialAction: [
+                {
+                  '@type': 'ViewAction',
+                  target: 'https://www.najmiter.dev/api/me',
+                },
+              ],
+            }),
+          }}
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              '@context': 'https://schema.org',
+              '@type': 'website',
               name: 'Najam ul Hassan',
               url: 'https://www.najmiter.dev/',
               logo: 'https://www.najmiter.dev/logo.png',
